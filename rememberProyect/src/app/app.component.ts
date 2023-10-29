bd295bc897330c00ce31998bcd910767be3e5c20
Emilio <emilio.agustin@hotmail.com>
2023-10-29 22:49:11 +0100

    primer commit

---
+++ rememberProyect/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooto',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rememberProyect';
+  test = 'Testeo'
+}
