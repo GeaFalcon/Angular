f8cb012523daf5ca828d4e86cab36d1c72d71d48
Emilio <emilio.agustin@hotmail.com>
2023-10-30 02:31:04 +0100

    primer commit

---
+++ rememberProyect/src/app/empleado/empleado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+  // template: " <div>Buenas</div>",
+  // styles: [
+  //     'p{background: red}'
+  //   ]
+})
+export class EmpleadoComponent {
+
+}
